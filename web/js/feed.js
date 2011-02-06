55748915b596f433990b138e4838b331e7bba01c
Graham Christensen <graham@grahamc.com>
2011-02-06 13:39:46 -0500

    adding feedago to the feed.

--- web/js/feed.js
+++ web/js/feed.js
@@ -5,9 +5,10 @@ function updateDataFeed() {
     $.get('/api/feed', {since: window.since}, function(data) {
         window.since = Math.round(new Date().getTime() / 1000);
 
-        $('#feed_replace').replaceWith(data);
+        $('#feed_replace').html(data);
+        $('#feed_replace').attr('id', '#feed_load_' + window.since);
         setTimeout('updateDataFeed();', 1000);
-        //jQuery(".timeago").timeago();
+        jQuery('#feed_load_' + window.since + " .timeago").timeago();
     }, 'html');
 }
 $(document).ready(function() { updateDataFeed(); });
