27ec289a72dd97028bdb5ea6af196b3eadcfa973
Graham Christensen <graham@grahamc.com>
2011-02-06 11:07:56 -0500

    Adding some auto-update javascript.

--- apps/frontend/modules/data/templates/_boxes.php
+++ apps/frontend/modules/data/templates/_boxes.php
@@ -1,17 +1,39 @@
-    <div class="boxes">
-        <div class="box rightbump">
-            <div class="totalusersheadline">Total Wings Consumed</div>
-            <div class="totalusersnumber"><?php echo $total; ?></div>
-        </div>
-
-        <div class="box">
-            <div class="totalusersheadline">WPH (wings per hour)</div>
-            <div class="totalusersnumber"><?php echo $rate; ?></div>
-        </div>
+<div class="boxes">
+    <div class="box rightbump">
+        <div class="totalusersheadline">Total Wings Consumed</div>
+        <div class="totalusersnumber" id="consumption_total"><?php echo $total; ?></div>
     </div>
 
-    <div class="boxes">
-        <div class="headerbox rightbump"><?php echo $remain; ?> Wings Remaining</div>
-
-        <div class="headerbox"><?php echo $need; ?> WPH Needed to Complete</div>
+    <div class="box">
+        <div class="totalusersheadline">WPH (wings per hour)</div>
+        <div class="totalusersnumber" id="wing_rate"><?php echo $rate; ?></div>
     </div>
+</div>
+
+<div class="boxes">
+    <div class="headerbox rightbump" id="consumption_remain"><?php echo $remain; ?> Wings Remaining</div>
+
+    <div class="headerbox" id="wing_need"><?php echo $need; ?> WPH Needed to Complete</div>
+</div>
+
+<script type="application/javascript">
+
+function updateDataCount() {
+    $.get('/api/count.json', function(data) {
+        $('#consumption_total').text(data.eaten);
+        $('#consumption_remaing').text(data.remaining);
+        setTimeout('updateDataCount();', 1000);
+    });
+}
+
+function updateDataRate() {
+    $.get('/api/rate.json', function(data) {
+        $('#wing_rate').text(data.current);
+        $('#wing_need').text(data.need);
+        setTimeout('updateDataRate();', 1000);
+    });
+}
+
+$(document).ready(function() { updateDataCount(); });
+$(document).ready(function() { updateDataRate(); });
+</script>
