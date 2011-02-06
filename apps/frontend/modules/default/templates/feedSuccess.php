0aae360451a9a7327ac0e82543979c3dcef44bc8
Graham Christensen <graham@grahamc.com>
2011-02-06 10:53:05 -0500

    Adding feed style.

--- apps/frontend/modules/default/templates/feedSuccess.php
+++ apps/frontend/modules/default/templates/feedSuccess.php
@@ -1,4 +1,9 @@
 <?php foreach ($feed as $item): ?>
-
-<?php echo $item->getUser() . ': ' . $item->getText(); ?><br />
+<div class="feedRow">
+	<div class="profPic"><img src="/uploads/<?php echo $item->getUser()->getPicture(); ?>" height="100px" width="100px"></div>
+	<div class="textBar">
+		<div class="barPointer"><img src="/images/pointer7.png"></div>
+		<div class="contentText"><span class="fatty"><?php echo $item->getUser(); ?></span> <?php echo $item->getText(); ?></div>
+	</div>
+</div>
 <?php endforeach; ?>
